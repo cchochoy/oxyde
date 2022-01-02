98f92f956ebcfe7b25ed2a0e36572af6fc122b44
cch <cch@cch.com>
2022-01-02 18:03:59 +0100

    first dumb kernel entrypoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic_handler(_info: &PanicInfo) -> ! {
+    loop {};
+}
+
+#[no_mangle]
+fn kernel_init() -> ! {
+    loop {};
 }
